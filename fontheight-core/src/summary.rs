6008e0c3bd9728465475074fdaaeb17ce945d244
Harry Dalton <harry.dalton@daltonmaag.com>
2025-03-04 12:15:52 +0000

    Refactor summary to use heaps to reduce runtime complexity
    Subsequent benchmarking suggests this was premature, but the intention
    is to guard against any future quadratic-like behaviour.

---
+++ fontheight-core/src/summary.rs
@@ -0,0 +1,109 @@
+use std::{cmp::Ordering, collections::BinaryHeap};
+
+use crate::Report;
+
+/// A summary of the lowest lows and highest highs.
+#[derive(Debug, Clone)]
+pub struct ReportSummary<'w> {
+    pub lowest: Vec<Report<'w>>,
+    pub highest: Vec<Report<'w>>,
+}
+
+/// A builder to construct a limited size summary from a stream of words. We do
+/// this as an explicit step with a binary heap for assured runtime complexity.
+#[derive(Debug, Clone)]
+pub(crate) struct ReportSummarizer<'w> {
+    lowest: BinaryHeap<ByLowest<'w>>,
+    highest: BinaryHeap<ByHighest<'w>>,
+}
+
+/// Report, but sorted ascending by lowest for a max-heap.
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+struct ByLowest<'w>(Report<'w>);
+
+/// Report, but sorted descending by highest for a min-heap.
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+struct ByHighest<'w>(Report<'w>);
+
+impl Ord for ByLowest<'_> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0.extremes.lowest.cmp(&other.0.extremes.lowest)
+    }
+}
+
+impl Ord for ByHighest<'_> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0
+            .extremes
+            .highest
+            .cmp(&other.0.extremes.highest)
+            .reverse()
+    }
+}
+
+impl PartialOrd for ByLowest<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        // Defer to Ord.
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialOrd for ByHighest<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        // Defer to Ord.
+        Some(self.cmp(other))
+    }
+}
+
+impl<'w> ReportSummarizer<'w> {
+    /// Build a summary with a maximum number of reports.
+    pub(crate) fn new(top_n: usize) -> Self {
+        Self {
+            lowest: BinaryHeap::with_capacity(top_n),
+            highest: BinaryHeap::with_capacity(top_n),
+        }
+    }
+
+    /// Consider adding a new report, if it is low and/or high enough.
+    pub(crate) fn push(&mut self, elem: Report<'w>) {
+        // Store if this report is stronger than the weakest report that would
+        // be evicted from the heap to accommodate it.
+        let by_lowest = ByLowest(elem);
+        if self.lowest.len() < self.lowest.capacity() {
+            self.lowest.push(by_lowest);
+        } else if let Some(mut weakest_low) = self.lowest.peek_mut() {
+            if by_lowest < *weakest_low {
+                *weakest_low = by_lowest;
+            }
+        }
+
+        // Store if this report is stronger than the weakest report that would
+        // be evicted from the heap to accommodate it.
+        let by_highest = ByHighest(elem);
+        if self.highest.len() < self.highest.capacity() {
+            self.highest.push(by_highest);
+        } else if let Some(mut weakest_high) = self.highest.peek_mut() {
+            if by_highest < *weakest_high {
+                *weakest_high = by_highest;
+            }
+        }
+    }
+
+    /// Consume this builder and produce the summary.
+    pub(crate) fn build(self) -> ReportSummary<'w> {
+        ReportSummary {
+            lowest: self
+                .lowest
+                .into_sorted_vec()
+                .into_iter()
+                .map(|by| by.0)
+                .collect(),
+            highest: self
+                .highest
+                .into_sorted_vec()
+                .into_iter()
+                .map(|by| by.0)
+                .collect(),
+        }
+    }
+}

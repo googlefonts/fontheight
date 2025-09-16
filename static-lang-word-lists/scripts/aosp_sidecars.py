d3e29fe7642485ec13ba31e4bdabb44319901b60
Simon Cozens <simon@simon-cozens.org>
2025-09-16 17:28:25 +0100

    Update sidecars script

--- static-lang-word-lists/scripts/aosp_sidecars.py
+++ static-lang-word-lists/scripts/aosp_sidecars.py
@@ -15,8 +15,12 @@ from iso639 import Language, LanguageNotFoundError
 AOSP_DIR = Path(__file__).parent.parent / "data" / "aosp"
 
 for word_list_path in AOSP_DIR.glob("*.txt"):
+    components = word_list_path.stem.split("_")  # lang_script
+    language_tag = components[0]
+    script = components[1] if len(components) == 2 else None
+
     try:
-        language = Language.from_part1(word_list_path.stem)
+        language = Language.from_part1(language_tag)
     except LanguageNotFoundError:
         print(
             f"removed {word_list_path.relative_to(AOSP_DIR.parent)}, language not recognised"
@@ -25,9 +29,11 @@ for word_list_path in AOSP_DIR.glob("*.txt"):
         continue
     name_clean = language.name.lower().replace(" (macrolanguage)", "").replace(" ", "_")
     doc = {
-        "name": f"aosp_{name_clean}",
-        "language": word_list_path.stem,
+        "name": f"aosp_{name_clean}" + (f"_{script}" if script else ""),
+        "language": language_tag,
     }
+    if script:
+        doc["script"] = script
     metadata_path = word_list_path.with_suffix(".toml")
     metadata_path.write_text(tomli_w.dumps(doc), encoding="utf-8")
     print(f"wrote {metadata_path.relative_to(AOSP_DIR.parent)}")

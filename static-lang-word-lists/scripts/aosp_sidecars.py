6373675cd8bb73dfa29ae98251f19a66c2c184ae
Ricky Atkins <ricky.atkins@daltonmaag.com>
2025-09-17 09:58:22 +0100

    Improve AOSP word list names
    Convert script tags to full names
    Switch from python-iso639 to babelfish
    Do more cleaning for names

--- static-lang-word-lists/scripts/aosp_sidecars.py
+++ static-lang-word-lists/scripts/aosp_sidecars.py
@@ -2,7 +2,7 @@
 # /// script
 # requires-python = ">=3.9"
 # dependencies = [
-#     "python-iso639",
+#     "babelfish>=0.6",
 #     "tomli-w",
 # ]
 # ///
@@ -10,30 +10,44 @@
 from pathlib import Path
 
 import tomli_w
-from iso639 import Language, LanguageNotFoundError
+from babelfish import Language, LanguageReverseError, Script
 
 AOSP_DIR = Path(__file__).parent.parent / "data" / "aosp"
 
 for word_list_path in AOSP_DIR.glob("*.txt"):
     components = word_list_path.stem.split("_")  # lang_script
     language_tag = components[0]
-    script = components[1] if len(components) == 2 else None
+    script = Script(components[1]) if len(components) == 2 else None
 
     try:
-        language = Language.from_part1(language_tag)
-    except LanguageNotFoundError:
+        language = Language.fromalpha2(language_tag)
+    except LanguageReverseError:
         print(
             f"removed {word_list_path.relative_to(AOSP_DIR.parent)}, language not recognised"
         )
         word_list_path.unlink()
         continue
-    name_clean = language.name.lower().replace(" (macrolanguage)", "").replace(" ", "_")
+
+    name_clean = (
+        language.name.lower()
+        .replace(" (macrolanguage)", "")
+        # Edge case for el (modern Greek)
+        .replace(" (1453-)", "")
+        .replace(" ", "_")
+    )
+    script_name_clean = (
+        "_" + script.name.lower().split(" ")[0] if script is not None else ""
+    )
+    # Avoid repeating script name if it's within the language name
+    if script_name_clean in "_" + name_clean:
+        script_name_clean = ""
+
     doc = {
-        "name": f"aosp_{name_clean}" + (f"_{script}" if script else ""),
+        "name": f"aosp_{name_clean}{script_name_clean}",
         "language": language_tag,
     }
     if script:
-        doc["script"] = script
+        doc["script"] = script.code
     metadata_path = word_list_path.with_suffix(".toml")
     metadata_path.write_text(tomli_w.dumps(doc), encoding="utf-8")
     print(f"wrote {metadata_path.relative_to(AOSP_DIR.parent)}")

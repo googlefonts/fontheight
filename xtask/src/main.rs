df7f230389e5c1b1bf249c6b5bc883996e513233
Ricky Atkins <ricky.atkins@daltonmaag.com>
2025-10-15 16:00:37 +0100

    Write existing build script

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,4 +1,18 @@
 #![allow(missing_docs)]
-fn main() {
-    println!("Hello, world!");
+
+use std::error::Error;
+
+use pico_args::Arguments;
+
+mod slwl_build;
+
+type Result<T = (), E = Box<dyn Error>> = std::result::Result<T, E>;
+
+fn main() -> Result {
+    let mut args = Arguments::from_env();
+
+    match args.subcommand()?.ok_or("missing task")?.as_str() {
+        "slwl-build" => slwl_build::main(args),
+        unknown => Err(format!("unknown task: {unknown}").into()),
+    }
 }
